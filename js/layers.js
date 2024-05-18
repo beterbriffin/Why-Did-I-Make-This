dcef617c3a6f163b13bc8392537ed475e9d9f509
beterbriffin <andrewpestle@gmail.com>
2024-05-18 16:19:38 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,7 +25,4 @@ addLayer("g", {
         {key: "g", description: "G: Reset for gyatt points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
-	    upgrades: {
-
-    },
 })
